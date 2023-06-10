eb5740e3a6568452a70ee1b90751bfbb15acba16
CHaNGo666 <84652788+CHaNGo666@users.noreply.github.com>
2023-06-10 03:05:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+
+  
+
+useEffect(()=>{
+
+axios.get("https://localhost:7006/WeatherForecast")
+.then(function (response){
+
+  console.log(response.data)
+})
+
+},[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <h1>Prueba conexion</h1>
+  
+  
+  </>
   );
 }
 
 export default App;
+
